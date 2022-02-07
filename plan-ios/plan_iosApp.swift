b7bb30855c8d61e6581fc2212aaf1267e798d723
Towelman-public <odagiritomoya@outlook.jp>
2022-02-07 11:36:03 +0900

    Initial Commit

---
+++ plan-ios/plan_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  plan_iosApp.swift
+//  plan-ios
+//
+//  Created by タオルマン on 2022/02/03.
+//
+
+import SwiftUI
+
+@main
+struct plan_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //TODO
+        }
+    }
+}
